65fdf190f2ff64712630b6df0af35864babf018c
Vinicius Goulart <vag.vinicius.goulart2006@gmail.com>
2024-01-03 18:37:25 -0300

    att

---
+++ main.js
@@ -0,0 +1,45 @@
+const listaConcluido = document.querySelector(".concluidos")
+const listaAndamento = document.querySelector(".em-andamento")
+
+cursosConcluidos = fetch("http://localhost:8000/concluidos")
+    .then(retorno => retorno.json())
+    .then(cursos => {
+        cursos.forEach(curso =>{
+
+            listaConcluido.appendChild(criarCard(curso.titulo, curso.link, curso.icone))
+
+        })
+    })
+
+cursosAndamento = fetch("http://localhost:3000/em-andamento")
+    .then(retorno => retorno.json())
+    .then(cursos => {
+        cursos.forEach(curso =>{
+            listaAndamento.appendChild(criarCard(curso.titulo, curso.certificado, curso.icone))
+
+        })
+    })
+
+
+function criarCard(titulo,certificado_link, icone_caminho){
+    const li = document.createElement("li")
+    li.classList.add("card")
+
+    const ancora = document.createElement("a")
+    ancora.setAttribute("href", certificado_link)
+    ancora.setAttribute("target", "_blank")
+
+    const tituloCard = document.createElement("h4")
+    tituloCard.classList.add("card__titulo")
+    tituloCard.textContent =  titulo
+
+    const iconeCard = document.createElement("img")
+    iconeCard.setAttribute("src", icone_caminho)
+    iconeCard.classList.add("card__img")
+
+    ancora.appendChild(tituloCard)
+    ancora.appendChild(iconeCard)
+    li.appendChild(ancora)
+
+    return li
+}
