0ff80ae48eca9094b3cd5536d8ffc3c60934d339
Vinicius Goulart <vag.vinicius.goulart2006@gmail.com>
2024-01-17 16:19:45 -0300

    correções e pequena atualização

---
+++ client/curriculoScript.js
@@ -0,0 +1,64 @@
+const listaConcluido = document.querySelector(".concluidos");
+const listaAndamento = document.querySelector(".em-andamento");
+
+const linkProducao = "http://127.0.0.1:8000";
+const linkDeploy = "https://portifolio-jpn3.onrender.com";
+
+
+
+    fetch(`${linkDeploy}/concluidos`)
+      .then((result) => result.json())
+      .then((cursos) => {
+        listaConcluido.textContent = "";
+        cursos.forEach((curso) => {
+          listaConcluido.appendChild(
+            criarCard(curso.titulo, curso.link, curso.icone)
+          );
+        });
+      })
+      .catch((error) => {
+        
+        listaAndamento.innerHTML = `Ocorreu um erro! (<code>${erro}</code>)`;
+      });
+
+
+    fetch(`${linkDeploy}/em-andamento`)
+      .then((result) => result.json())
+      .then((cursos) => {
+        listaConcluido.textContent = "";
+        cursos.forEach((curso) => {
+          listaConcluido.appendChild(
+            criarCard(curso.titulo, curso.link, curso.icone)
+          );
+        });
+      })
+      .catch((error) => {
+        listaAndamento.innerHTML = `Ocorreu um erro! (<code>${error}</code>)`;
+      });
+      
+
+
+
+function criarCard(titulo, certificado_link, icone_caminho) {
+  const li = document.createElement("li");
+  li.classList.add("lista-item");
+
+  const ancora = document.createElement("a");
+  ancora.setAttribute("href", certificado_link);
+  ancora.setAttribute("target", "_blank");
+  ancora.classList.add("card")
+
+  const tituloCard = document.createElement("h4");
+  tituloCard.classList.add("card__titulo");
+  tituloCard.textContent = titulo;
+
+  const iconeCard = document.createElement("img");
+  iconeCard.setAttribute("src", icone_caminho);
+  iconeCard.classList.add("card__img");
+
+  ancora.appendChild(tituloCard);
+  ancora.appendChild(iconeCard);
+  li.appendChild(ancora);
+
+  return li;
+}
---
+++ client/projetosScript.js
@@ -0,0 +1,5 @@
+const btnCard = document.querySelector(".projeto__card__icone");
+
+btnCard.addEventListener("click", () =>
+  window.location.replace("https://github.com/Venux115/cronometro")
+);
